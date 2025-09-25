8908d2e4a86fa5ca4fb6708cb725391e6864adb8
trubiso <33161454+trubiso@users.noreply.github.com>
2025-09-25 15:35:02 +0200

    scrolling background

--- main.py
+++ main.py
@@ -43,31 +43,43 @@ class Game:
 			game_object.draw(self.screen)
 		# self.player.draw(self.screen)
 		# self.enemy.draw(self.screen)
-		# self.screen.blit(sprites.PIXEL_PLATFORMER_BACKGROUNDS.get_tile_at(4, 0), (0, 0))
-		# self.screen.blit(sprites.PIXEL_PLATFORMER_BACKGROUNDS.get_tile_at(4, 1), (0, 24))
-		# self.screen.blit(sprites.PIXEL_PLATFORMER_BACKGROUNDS.get_tile_at(4, 2), (0, 48))
-		# self.screen.blit(sprites.PIXEL_PLATFORMER_BACKGROUNDS.get_tile_at(4, 3), (0, 24*3))
 
 class BackgroundController(GameObject):
-	def __init__(self, tile_manager: TileManager, i_start = 0, i_qty = 4, repeat_h = 1, repeat_v = 1, j_start = 0, j_qty = 3):
+	def __init__(self, tile_manager: TileManager, i_start = 0, i_qty = 4, repeat_h = 1, repeat_v = 1, scroll = Vector2.zero(), scroll_freq = 10.0, j_start = 0, j_qty = 3):
 		self.tile_size = 24
 		self.tile_manager = tile_manager
 		self.i_start = i_start
 		self.i_qty = i_qty
 		self.repeat_h = repeat_h
 		self.repeat_v = repeat_v
+		self.scroll = scroll
+		self.scroll_freq = scroll_freq
+		self.scrolls = scroll != Vector2.zero()
 		self.j_start = j_start
 		self.j_qty = j_qty
 		self.surface = pygame.Surface((self.tile_size * i_qty * repeat_h, self.tile_size * j_qty * repeat_v))
+		self.offset = Vector2.zero()
+		self.scroll_ctr = 0
 	
 	def draw(self, win: pygame.Surface):
+		self.surface.fill(pygame.Color(0, 0, 0))
+
 		for i in range(self.i_start, self.i_start + self.i_qty):
 			for j in range(self.j_start, self.j_start + self.j_qty):
-				for h in range(self.repeat_h):
-					for v in range(self.repeat_v):
-						self.surface.blit(self.tile_manager.get_tile_at(i, j), ((i - self.i_start + h) * 24, (j - self.j_start + v) * 24))
+				tile = self.tile_manager.get_tile_at(i, j)
+				for h in range(-1 if self.scrolls else 0, self.repeat_h + (1 if self.scrolls else 0)):
+					for v in range(-1 if self.scrolls else 0, self.repeat_v + (1 if self.scrolls else 0)):
+						self.surface.blit(tile, ((i - self.i_start + h * self.i_qty) * 24 + self.offset.x, (j - self.j_start + v * self.j_qty) * 24 + self.offset.y))
 		
 		pygame.transform.scale(self.surface, (640, 480), win)
+	
+	def update(self):
+		self.scroll_ctr += 1
+		if self.scroll_ctr == self.scroll_freq:
+			if self.scrolls:
+				self.offset += self.scroll
+				self.offset = self.offset.wrap(Vector2.zero(), Vector2(self.tile_size * self.i_qty, self.tile_size * self.j_qty))
+			self.scroll_ctr = 0
 
 # class player:
 
@@ -82,6 +94,6 @@ class Enemy:
 		pass
 
 game = Game(640, 480, PygameController(), 60)
-background_controller = BackgroundController(sprites.PIXEL_PLATFORMER_BACKGROUNDS, 4, 2, 2, 2)
+background_controller = BackgroundController(sprites.PIXEL_PLATFORMER_BACKGROUNDS, 4, 2, 2, 1, Vector2(1, 0))
 game.add_game_object(background_controller)
 game.run()
--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
 def lerp(start, end, strength):
 	return (1 - strength) * start + strength * end
+
+def clamp(value, minimum, maximum):
+	return min(max(value, minimum), maximum)
+
+def wrap(value, minimum, maximum):
+	while value > maximum:
+		value -= (maximum - minimum)
+	while value < minimum:
+		value += (maximum - minimum)
+	return value
