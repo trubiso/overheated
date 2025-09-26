b66ad3963965a44062d25bf7ef0a92c356b5a6db
nikkieesm <simutkinaveronika7@gmail.com>
2025-09-26 02:08:28 +0200

    def draw + enemy.py passado a folder overheated

--- enemy.py
+++ enemy.py
@@ -4,6 +4,8 @@ import pygame
 
 
 class Enemy(KinematicObject):
+
+    walk = pygame.image.load('sprites/pixel_platformer_pack/tiles/tile_0000.png')
     def __init__(self, position: Vector2, imagen: pygame.Surface):
         super().__init__(position, Vector2.from_tuple(imagen.get_size()))
         self.base_speed = 1
@@ -15,7 +17,14 @@ class Enemy(KinematicObject):
     def draw(self, win):
         self.move()
         if self.walk_count + 1 >= 33:
-            pass
+            self.walk_count = 0
+
+        if self.base_speed>0:
+            win.blit(self.walk[self.walk_count//3],(self.position))
+            self.walk_count += 1
+        else:
+            win.blit(self.walk[self.walk_count//3],(self.position))
+            self.walk_count += 1
 
     def move(self):
         pass
