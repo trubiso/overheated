8fc810956d0bde671834a3a307260fbcce67f04c
trubiso <33161454+trubiso@users.noreply.github.com>
2025-09-30 09:53:56 +0200

    remove random line of code

--- objects/enemy.py
+++ objects/enemy.py
@@ -5,21 +5,19 @@ from tilemap import TileManager
 
 
 class Enemy(KinematicObject):
+	def __init__(self, position: Vector2, size: Vector2):
+		super().__init__(position, size)
+		self.base_speed = 1
+		self.health = 3
+		self.walk_count = 0
+		sprite = pygame.image.load('sprites/pixel_platformer_pack/tiles/tile_0000.png')
+		self.imagen = pygame.transform.scale(sprite, self.size.to_tuple())
 
-    walk = pygame.image.load('sprites/pixel_platformer_pack/tiles/tile_0000.png')
-    def __init__(self, position: Vector2, size: Vector2):
-        super().__init__(position, size)
-        self.base_speed = 1
-        self.health = 3
-        self.walk_count = 0
-        sprite = pygame.image.load('sprites/pixel_platformer_pack/tiles/tile_0000.png')
-        self.imagen = pygame.transform.scale(sprite, self.size.to_tuple())
+	def draw(self, win: pygame.Surface):
+		win.blit(self.imagen, (self.position.x, self.position.y, self.size.x, self.size.y))
 
-    def draw(self, win: pygame.Surface):
-        win.blit(self.imagen, (self.position.x, self.position.y, self.size.x, self.size.y))
+	def move(self):
+		pass
 
-    def move(self):
-        pass
-
-    def update(self, controller, dt):
-        pass
+	def update(self, controller, dt):
+		pass
